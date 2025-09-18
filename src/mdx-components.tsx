19f928a00221aa553bdce5fb7903e929457a12d0
Julian Koehn <me@julian.pro>
2025-09-18 13:58:25 +0200

    fix: linting issues

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,18 +1,18 @@
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
-import { Mermaid } from "./components/mdx/mermaid";
 import { Accordion, Accordions } from "./components/accordion";
+import { Mermaid } from "./components/mdx/mermaid";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-  return {
-    ...defaultMdxComponents,
-    // biome-ignore lint/suspicious/noExplicitAny: fumadocs
-    img: (props) => <ImageZoom {...(props as any)} />,
-    Mermaid,
-    Accordions,
-    Accordion,
-    ...components,
-  };
+	return {
+		...defaultMdxComponents,
+		// biome-ignore lint/suspicious/noExplicitAny: fumadocs
+		img: (props) => <ImageZoom {...(props as any)} />,
+		Mermaid,
+		Accordions,
+		Accordion,
+		...components,
+	};
 }
