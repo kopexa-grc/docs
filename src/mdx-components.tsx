72606e3b395d7828cd407b3f66329ec2d40dc3f7
Julian Koehn <me@julian.pro>
2025-09-18 13:47:42 +0200

    feat: status documentation

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,14 +2,17 @@ import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { Mermaid } from "./components/mdx/mermaid";
+import { Accordion, Accordions } from "./components/accordion";
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
-	return {
-		...defaultMdxComponents,
-		// biome-ignore lint/suspicious/noExplicitAny: fumadocs
-		img: (props) => <ImageZoom {...(props as any)} />,
-		Mermaid,
-		...components,
-	};
+  return {
+    ...defaultMdxComponents,
+    // biome-ignore lint/suspicious/noExplicitAny: fumadocs
+    img: (props) => <ImageZoom {...(props as any)} />,
+    Mermaid,
+    Accordions,
+    Accordion,
+    ...components,
+  };
 }
